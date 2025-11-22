dfd636cc164cb8200c4d1ba661a7fb0adb05cf21
Vijaya <Shori.Levii@gmail.com>
2025-11-22 16:37:55 +0530

    Terraform/staging

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 variable "env" {
-  default = "prod"
+  default = "staging"
 }
 
 variable "region" {
-  default = "us-east1"
+  default = "us-central1"
 }
 
 variable "public_subnet_cidr" {
-  default = "10.2.1.0/24"
+  default = "10.1.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-  default = "10.2.2.0/24"
+  default = "10.1.2.0/24"
 }
 
 variable "project_id" {
-  default = "<PROD_PROJECT_ID>"
+  default = "<STAGING_PROJECT_ID>"
 }
